b3a571d7ad02c0bf94df8473f8ad2a56cddd2a32
khuongnm0 <khuongnm0@gmail.com>
2020-05-28 18:46:47 +0700

    Add Leaftlet map. Change color range to Green to Red

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -1,40 +1,113 @@
-// HOMEPAGE JSvar map;
+// HOMEPAGE
+
+// Helper Load JSON from Local path
+async function loadJSON(path) {
+  const res = await fetch(path);
+  return await res.json();
+}
+
+function getGreenToRed(percent) {
+  let r = percent < 50 ? 255 : Math.floor(255 - (percent * 2 - 100) * 255 / 100);
+  let g = percent > 50 ? 255 : Math.floor((percent * 2) * 255 / 100);
+  return 'rgb(' + r + ',' + g + ',0)';
+}
+
+// function invoke when callback from google API
 function initMap() {
-  const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: { lat: 50, lng: 0 }
+  const mapGoogle = new google.maps.Map(document.getElementById('mapGoogle'), {
+    zoom: 6,
+    center: { lat: 53, lng: -2 } // England
   });
 
-  console.log('asda');
+
   // Load GeoJSON.
-  map.data.loadGeoJson(
-    'https://raw.githubusercontent.com/martinjc/UK-GeoJSON/master/json/electoral/eng/wpc.json');
+  mapGoogle.data.loadGeoJson(
+    './assets/json/wpc.json'
+  );
 
   // Set the stroke width, and fill color for each polygon
-  map.data.setStyle(
-    //   {
-    //   fillColor: 'green',
-    //   strokeWeight: 1
-    // }
+  mapGoogle.data.setStyle(
     function (feature) {
-      var PCON13CD = feature.getProperty('PCON13CD');
-      var PCON13CDO = feature.getProperty('PCON13CDO');
-      var PCON13NM = feature.getProperty('PCON13NM');
-      var color = '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
+      // let color = '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
+
+      let percent = Math.floor(Math.random() * 100);
+      // console.log('percent: ', percent);
+
+      let color = getGreenToRed(percent)
+      // console.log('color: ', color);
 
       return {
         fillColor: color,
         strokeWeight: 1
+
       };
     }
   );
 
   // Add listener click event
-  map.data.addListener('click', function (event) {
-    var newColor = '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
-    console.log('newColor: ', newColor);
+  mapGoogle.data.addListener('click', function (event) {
 
-    map.data.overrideStyle(event.feature, { fillColor: newColor }); // override current style with new color
+    let PCON13CD = event.feature.getProperty('PCON13CD');
+    let PCON13CDO = event.feature.getProperty('PCON13CDO');
+    let PCON13NM = event.feature.getProperty('PCON13NM');
+
+    console.log('PCON13CD: ', PCON13CD);
+    console.log('PCON13CDO: ', PCON13CDO);
+    console.log('PCON13NM: ', PCON13NM);
+
+    // let percent = Math.floor(Math.random() * 100);
+    // console.log('percent: ', percent);
+
+    // let newColor = getGreenToRed(percent)
+    // console.log('color: ', newColor);
+
+    // mapGoogle.data.overrideStyle(event.feature, { fillColor: newColor }); // override current style with new color
 
   });
+
+
 }
+
+
+function initMapLeaflet() {
+  const mapLeaflet = L.map('mapLeaflet', {
+    center: [53, -2],
+    zoom: 6
+  });
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(mapLeaflet);
+
+  // L.marker([51.5, -0.09]).addTo(mapLeaflet)
+  //   .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+  //   .openPopup();
+
+
+
+  loadJSON('./assets/json/wpc.json').then(data => {
+    const dataJSON = data;
+    console.log('dataJSON: ', dataJSON);
+
+    L.geoJSON(dataJSON,
+      {
+        style: function (feature) {
+          let percent = Math.floor(Math.random() * 100);
+          let color = getGreenToRed(percent)
+          return {
+            fillColor: color,
+            color: '#000', // Stroke color
+            weight: 1 // Stroke weight
+          };
+        }
+      }
+    ).addTo(mapLeaflet);
+  });
+
+
+}
+
+
+
+initMapLeaflet();
+
